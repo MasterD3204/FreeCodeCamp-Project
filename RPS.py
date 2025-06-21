93b0659c3927b612eb6e2e468e4962208b51949c
Dũng Đỗ Quang <22022561@vnu.edu.vn>
2025-06-21 17:12:58 +0000

    Add new player

--- RPS.py
+++ RPS.py
@@ -1,10 +1,63 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. 
+# It is not a very good player so you will need to change the code to pass the challenge.
+import random
+from collections import Counter
 
-def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+# beat quincy
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+class QuincySlayer():
+    def __init__(self):
+        self.turn_counter = 0
+        self.quincy_patterns = ["R", "R", "P", "P", "S"]
+        self.counter_quincy = {"P":"S","R":"P","S":"R"}
+    
+    def make_move(self, prev_play, opponent_history=[]):
+        predict_move = self.quincy_patterns[self.turn_counter % len(self.quincy_patterns)]
+        self.turn_counter += 1
+        my_move = self.counter_quincy[predict_move]
+        return my_move
 
-    return guess
+
+class KrisSlayer():
+    def __init__(self):
+        self.last_move = None
+        self.counter = {"P":"S","R":"P","S":"R"}
+
+    def make_move(self, prev_play, opponent_history=[]):
+        my_move = ''
+        if self.last_move is None:
+            my_move = 'R'
+        else:
+            kris_move = self.counter[self.last_move]
+            my_move = self.counter[kris_move]
+        self.last_move = my_move
+        return my_move
+
+class MrugeshSlayer():
+    def __init__(self):
+        self.history = []
+        self.counter = {"P":"S","R":"P","S":"R"}
+        self.move = ['R','P','S']
+
+    def make_move(self, prev_play, opponent_history=[]):
+        my_move = ''
+        if not self.history:
+            my_move = 'S'
+        else:
+            bait_move = self.move[len(self.history)%3]
+            current_history = (self.history + [bait_move])[-10:]
+
+            counts = Counter(current_history)
+
+            
+            most_frequent = counts.most_common(1)[0][0]
+    
+            predict_mru_move = self.counter[most_frequent]
+            my_move = self.counter[predict_mru_move]
+        self.history.append(my_move)
+        return my_move
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 # This entrypoint file to be used in development. Start by reading README.md
 from RPS_game import play, mrugesh, abbey, quincy, kris, human, random_player
-from RPS import player
+#from RPS import player
 from unittest import main
+from RPS import QuincySlayer, KrisSlayer, MrugeshSlayer
 
-play(player, quincy, 1000)
-play(player, abbey, 1000)
-play(player, kris, 1000)
-play(player, mrugesh, 1000)
+bot = KrisSlayer()
+
+play(bot.make_move, quincy, 1000)
+play(bot.make_move, abbey, 1000)
+play(bot.make_move, kris, 1000)
+play(bot.make_move, mrugesh, 1000)
 
 # Uncomment line below to play interactively against a bot:
 # play(human, abbey, 20, verbose=True)
