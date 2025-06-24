9e1f8fe91355418fc70f4eb88bc5c67640eff40a
MasterD3204 <22022561@vnu.edu.vn>
2025-06-25 00:52:03 +0700

    add Markov bot

--- RPS.py
+++ RPS.py
@@ -1,63 +1,45 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. 
 # It is not a very good player so you will need to change the code to pass the challenge.
 import random
-from collections import Counter
+R, P, S = 'R', 'P', 'S'
+MOVES = [R, P, S]
 
-# beat quincy
+class MarkovBot():
+    def __init__(self, order=2):
+        self.order = order
+        self.counter = {P: S, R: P, S: R} 
+        self.history = []      
+        self.transitions = {}     
 
-class QuincySlayer():
-    def __init__(self):
-        self.turn_counter = 0
-        self.quincy_patterns = ["R", "R", "P", "P", "S"]
-        self.counter_quincy = {"P":"S","R":"P","S":"R"}
-    
-    def make_move(self, prev_play, opponent_history=[]):
-        predict_move = self.quincy_patterns[self.turn_counter % len(self.quincy_patterns)]
-        self.turn_counter += 1
-        my_move = self.counter_quincy[predict_move]
-        return my_move
+    def predict_opponent_move(self):
 
+        if len(self.history) < self.order:
+            return None 
+        
+        last_sequence = "".join(self.history[-self.order:])
 
-class KrisSlayer():
-    def __init__(self):
-        self.last_move = None
-        self.counter = {"P":"S","R":"P","S":"R"}
+        if last_sequence not in self.transitions:
+            return None 
 
-    def make_move(self, prev_play, opponent_history=[]):
-        my_move = ''
-        if self.last_move is None:
-            my_move = 'R'
+        # Tìm nước đi có khả năng xảy ra cao nhất
+        prediction_counts = self.transitions[last_sequence]
+        prediction = max(prediction_counts, key=prediction_counts.get)
+        return prediction
+
+    def playy(self, last_opponent_move=""):
+        if last_opponent_move != "" and last_opponent_move in MOVES:
+            if len(self.history) >= self.order:
+                last_sequence = "".join(self.history[-self.order:])
+                if last_sequence not in self.transitions:
+                    self.transitions[last_sequence] = {R: 0, P: 0, S: 0}
+                # Ghi nhận nước đi của đối thủ
+                self.transitions[last_sequence][last_opponent_move] += 1
+            self.history.append(last_opponent_move)
+
+        opponent_prediction = self.predict_opponent_move()
+        my_move = ""
+        if opponent_prediction is not None:
+            my_move = self.counter[opponent_prediction]
         else:
-            kris_move = self.counter[self.last_move]
-            my_move = self.counter[kris_move]
-        self.last_move = my_move
+            my_move = random.choice(MOVES) 
         return my_move
-
-class MrugeshSlayer():
-    def __init__(self):
-        self.history = []
-        self.counter = {"P":"S","R":"P","S":"R"}
-        self.move = ['R','P','S']
-
-    def make_move(self, prev_play, opponent_history=[]):
-        my_move = ''
-        if not self.history:
-            my_move = 'S'
-        else:
-            bait_move = self.move[len(self.history)%3]
-            current_history = (self.history + [bait_move])[-10:]
-
-            counts = Counter(current_history)
-
-            
-            most_frequent = counts.most_common(1)[0][0]
-    
-            predict_mru_move = self.counter[most_frequent]
-            my_move = self.counter[predict_mru_move]
-        self.history.append(my_move)
-        return my_move
-
-
-
-
-
